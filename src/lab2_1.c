8e02a8af28096523146b6dce7a321d30f415eaf6
Mathisgg-dev <matguillin06@gmail.com>
2026-09-23 13:02:10 +0000

    complete lab2 tasks

--- src/lab2_1.c
+++ src/lab2_1.c
@@ -12,17 +12,31 @@
 */
 
 int sum_to_n(int n) {
-    // TODO: implement sum with a for loop
-    return 0; // placeholder
+  // TODO: implement sum with a for loop
+  int sum = 0;
+  for (int i = 1; i <= n; i++) {
+    sum = sum + i;
+  }
+
+  return sum;  // placeholder
 }
 
 int main(void) {
-    int n;
+  int n;
 
-    printf("Enter a positive integer n: ");
-    scanf("%d", &n);
+  printf("Enter a positive integer n: ");
+  scanf("%d", &n);
 
-    // TODO: validate input, call function, and print result
+  if (n < 1) {
+    printf("Error");
+  }
 
-    return 0;
+  else {
+    int result = sum_to_n(n);
+    printf("The sum from 1 to %d is %d\n", n, result);
+  }
+
+  // TODO: validate input, call function, and print result
+
+  return 0;
 }
